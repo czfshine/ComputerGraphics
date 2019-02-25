6cb0db58d3354426aa5bd76157286882f99078f2
chen zefan <czfshine@outlook.com>
2019-02-25 17:04:03 +0800

    point.cpp

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # ComputerGraphics
-一些计算机图形学相关的代码，从基础到深入
+一些计算机图形学相关的代码，主要内容是在学<计算机图形学>这门课和opengl的过程中的一些代码片段和程序。
+
+
+
+## Index
+
+> 代码在src文件夹下，各个代码的简要说明在下面
+
+* basic 基础
+  * `point.cpp` 一个点
+
+## Build
+
+整个项目使用**cmake**构建，按照传统的cmake编译流程编译就行，需要用到的外部库有`opengl`，`glew`，`freeglut`.
+
+在Ubuntu 1804和Windows 10 下都测试通过。
+
+下面是具体的编译流程
+
+### Linux
+
+```bash
+cmake ./
+make
+```
+
+完事
+
+
+
+### Window
+
+先配置外部库，再生成Visual Studio 2017 的解决方案文件：
+
+```bash
+cmake -G "Visual Studio 15"
+```
+
+然后在VS2017打开生成的解决方案文件，构建就行. 
+
+
+
+## Screenshot
+
+![point.cpp](./screenshot/Screenshot from 2019-02-25 16-54-53.png)
---
+++ src/basic/point.cpp
@@ -0,0 +1,33 @@
+//基础:利用opengl绘制一个点
+// Created by czfshine on 19-2-25.
+//
+
+#include <GL/glut.h>
+
+/**
+ * 主要的绘制函数
+ * 因为这个程序是静态而且没有交互，所以只要这一个函数就行
+ * @return
+ */
+void Display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPointSize(5.0f); //大小
+    glBegin(GL_POINTS); //表明开始画点
+    glVertex2f(0.5f,0.8f);//屏幕分为x（-1,1）y（-1,1）的矩形
+    glEnd(); //表明结束绘画点，也就是可以画别的东西了
+    glFlush(); //注意，和写文件一样有缓冲区的，要更新才能真正的显示
+}
+
+int main(int argc,char* argv[])
+{
+    //一些初始化窗口和opengl的函数
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_RGB);
+    glutInitWindowPosition(200,200);
+    glutInitWindowSize(800,800);
+    glutCreateWindow("The First Point");
+    glutDisplayFunc(&Display);
+    glutMainLoop();
+    return 0;
+}
